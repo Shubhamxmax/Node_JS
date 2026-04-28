ae5178f3e22c6d5c4a72cccabc648b98079a739d
Shubhamxmax <shubh101bhagat@gmail.com>
2026-04-28 19:48:18 +0530

    Learned Methods of HTTP

--- Hello-World/Server/index.js
+++ Hello-World/Server/index.js
@@ -1,22 +1,39 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   // console.log("New Req Recorded");
   // console.log(req);// give all the information of client or user
-  if( req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} New Req Received\n`;
+
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
+
   // fs.appendFile('log.txt',log,(err,data)=>{
   //     res.end("Hello From  shubham's Server")
   // })
+
+  const myUrl = url.parse(req.url, true); //It converts the URL(req.url) into an object with different key–value pairs
+
+  // console.log(myUrl)
+
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
-        res.end("HomePage");
+        if (req.method === "GET") res.end("HomePage");
         break;
       case "/about":
-        res.end("I am Shubham");
+        const username = myUrl.query.myname;
+        res.end(`Hi ${username}`);
         break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here are your results from " + search);
+      case "/signup":
+        if (req.method === "GET") res.end("This is a signupform");
+        else if (req.method === "POST") res.end("Success");
+        break;
+
       default:
         res.end("404 Not Found");
     }
