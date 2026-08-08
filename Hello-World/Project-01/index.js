f9c625c2a052a9b0c98a454100ac6bbeb28e9be9
Shubhamxmax <shubh101bhagat@gmail.com>
2026-08-09 02:26:22 +0530

    Learned Middleware and Postman

--- Hello-World/Project-01/index.js
+++ Hello-World/Project-01/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json"); // no export needed in .json file
+const fs = require('fs')
 
 const app = express();
 const PORT = 5000;
 
-// Routes
+// // Routes
 
 //SSR(Server Side Rendering):Sends html read-made page 
 app.get("/users", (req, res) => {
@@ -16,31 +17,95 @@ app.get("/users", (req, res) => {
   res.send(html);
 });
 
-// REST API
-//CSR(Client Side Rendering):Sends only json 
+// // REST API
+// //CSR(Client Side Rendering):Sends only json 
+// app.get("/api/users", (req, res) => {
+//   return res.json(users);
+// });
+
+// app.get("/api/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
+
+//   users.forEach(user => {
+//     if (user.id === id) {
+//       return res.json(user.first_name);
+//     }
+//   });
+// });
+
+// //SSR(Server Side Rendering):Sends html read-made page 
+// app.get("/users", (req, res) => {
+//   const html = `
+//       <ul>
+//         ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+//       </ul>
+//     `;
+//   res.send(html);
+// });
+
+
+
+
+
+// Part 2
+
+
+//Middleware - Plugin
+
+app.use(express.urlencoded({ extended: false})); 
+
+
+app.use((req, res, next) => {
+    fs.appendFile(
+        "log.txt",
+        `${Date.now()}: ${req.method}: ${req.path}\n`,
+        (err) => {
+            next();
+        }
+    );
+});
+
+app.use((req, res, next) =>{
+    console.log("Hello from MiddleWare 3");
+    // return res.json({msg: " Hello from Middleware 3"});
+    next();
+})
+
+
 app.get("/api/users", (req, res) => {
-  return res.json(users);
+    console.log(req.headers); // seeing request headers
+    res.setHeader("myName", "Piyush Garg"); // creating my own header
+    return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
+app
+    .route("/api/users/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
 
-  users.forEach(user => {
-    if (user.id === id) {
-      return res.json(user.first_name);
-    }
-  });
+        const user = users.find((user) => user.id === id);
+
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        // Edit user with id
+        return res.json({ status: "Pending" });
+    })
+    .delete((req, res) => {
+        // Delete user with id
+        return res.json({ status: "Pending" });
+    });
+
+    app.post("/api/users", (req, res) => {
+    // TODO: Create new user
+    const body = req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "pending" , id: users.length});
+});
 });
 
-//SSR(Server Side Rendering):Sends html read-made page 
-app.get("/users", (req, res) => {
-  const html = `
-      <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-      </ul>
-    `;
-  res.send(html);
-});
+
 
 app.listen(PORT, () => {
   console.log("Server Started");
